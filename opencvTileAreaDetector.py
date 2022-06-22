7155b98505400655b321571127a53e5ee1fa6843
exape <contactme.exape@gmail.com>
2022-06-22 16:49:10 +0200

    code cleanup

--- opencvTileAreaDetector.py
+++ opencvTileAreaDetector.py
@@ -1,59 +1,53 @@
 import cv2 as cv
-from cv2 import pencilSketch
 import numpy as np
 import pyautogui
-import math
 import time
-import mss 
+import mss
 
+# global variables
+kernel = cv.imread("image.png")
+monitor_number = 1
 
-def doscreenshot():
+#   doscreenshot()
+# creates a screenshot based on the monitor number
+# monitor 1 = adrien
+# monitor 2 = flavien, lucas
+def doscreenshot(monitornumber):
     with mss.mss() as sct:
-        # get information of monitor 1(adrien) or monitor 2(flavien, lucas)
-        monitor_number = 2
-        mon = sct.monitors[monitor_number]
+        mon = sct.monitors[monitornumber]
         monitor = {
             "top": mon["top"],
             "left": mon["left"],
             "width": mon["width"],
             "height": mon["height"],
-            "mon": monitor_number,
+            "mon": monitornumber,
         }
-
         # Grab the image screen
         img = np.array(sct.grab(monitor))
-
         return img.astype(np.uint8)
 
 
-
-kernel = cv.imread('image.png')
-test = cv.imread("test.png")
-def findmatch():
-
-    # get screenshot
-    img = doscreenshot()
-    img = img[:,:,0:3]
-
+#   findtile()
+# finds tile based on the image provided
+def findtile(imageprovided):
+    img = imageprovided[:, :, 0:3]
     # fill black on sides to fix hud problem
     for i in range(15):
         img[:, i] = 0
         img[i, :] = 0
         img[:, len(img[0]) - i - 1] = 0
         img[len(img) - i - 1, :] = 0
-        
+    # find the pattern based on the image provided
     res = cv.matchTemplate(img, kernel, cv.TM_CCOEFF_NORMED)
-    # cv.imshow('res',res)
-    # cv.waitKey()
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    _, max_val, _, max_loc = cv.minMaxLoc(res)
     threshold = 0.8
-    print(max_loc,max_val)
+    print(max_loc, max_val)
+    # if found, right click on it
     if max_val >= threshold:
-        print('Found needle.')
-    pyautogui.click(button='right',x=max_loc[0]+1920,y=max_loc[1])
+        print("Found needle.")
+    pyautogui.click(button="right", x=max_loc[0], y=max_loc[1])
 
 
-
-# program itself
-
-findmatch()
+# main
+time.sleep(3)
+findtile(doscreenshot(monitor_number))
