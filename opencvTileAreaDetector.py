2be54d682ff86400184324f011ce8624d462bc96
exape <contactme.exape@gmail.com>
2022-06-24 18:04:25 +0200

    check if player is harvesting

--- opencvTileAreaDetector.py
+++ opencvTileAreaDetector.py
@@ -1,4 +1,3 @@
-import click
 import cv2 as cv
 import numpy as np
 import pyautogui
@@ -7,8 +6,14 @@ import mss
 import math
 
 #   global variables
-monitornumber = 1
-middleofthescreen = [960, 540]
+monitor_number = 1
+middle_of_the_screen = [960, 540]
+template_button = cv.imread("images/template_button.png", 0)
+template_ressource = cv.imread("images/template_ressource.png", 0)
+template_mining = cv.imread("images/template_mining_in_progress.png", 0)
+template_mining_while_click = cv.imread("images/template_click_while_mining.png", 0)
+something_in_the_area = 1
+harvesting = 0
 
 #   doscreenshot()
 # creates a screenshot based on the monitor number
@@ -22,34 +27,31 @@ def doscreenshot(monitornb):
             "left": mon["left"],
             "width": mon["width"],
             "height": mon["height"],
-            "mon": monitornumber,
+            "mon": monitornb,
         }
         img = np.array(sct.grab(monitor))
+        # needs to remove the hud on the bottom of screenshot with black pixels
         return img.astype(np.uint8)
 
 
-#   findtile()
-# finds tile based on the image provided and clicks on it
-def findtile(imageprovided):
+#   find()
+# finds occurence based on the image provided
+def find(imageprovided, template, tres):
     imgrgb = imageprovided
     imggray = cv.cvtColor(imgrgb, cv.COLOR_RGB2GRAY)
-    template = cv.imread("images/template.png", 0)
     res = cv.matchTemplate(imggray, template, cv.TM_CCOEFF_NORMED)
-    threshold = 0.8
+    threshold = tres
     loc = np.where(res >= threshold)
     istheretiles = len(loc[0])
     coords = []
-
     if istheretiles > 1:
-        print("ressource detected")
+        print("something to collect detected")
         for pt in zip(*loc[::-1]):
             coords += [(pt[0], pt[1])]
-        print("i found the following coordinates: ", coords)
         coords.sort(key=lambda x: math.sqrt((x[0] - 960) ** 2 + (x[1] - 540) ** 2))
         print("clicking on the closest one: ", coords[0])
         return coords[0]
     else:
-        print("no ressource detected, let's move on.")
         return None
 
 
@@ -59,11 +61,53 @@ def clicktile(coordinates):
     pyautogui.click(button="right", x=coordinates[0], y=coordinates[1])
 
 
-#   main
+#   clickharvest()
+# clicks on the button to harvest the ressource by a coordinates tuple as a parameter
+def clickharvest(coordinates):
+    pyautogui.click(button="left", x=coordinates[0], y=coordinates[1])
+
+
+#   isharvesting()
+# finds if the bot is harvesting based on the image provided
+def isharvesting(imageprovided, template, tres):
+    imgrgb = imageprovided
+    imggray = cv.cvtColor(imgrgb, cv.COLOR_RGB2GRAY)
+    res = cv.matchTemplate(imggray, template, cv.TM_CCOEFF_NORMED)
+    threshold = tres
+    loc = np.where(res >= threshold)
+    istheretiles = len(loc[0])
+    if istheretiles > 1:
+        return True
+    else:
+        return False
+
+
+def explorezone():
+    print("exploring zone")
+    global something_in_the_area
+    while something_in_the_area:
+        global harvesting
+        harvesting = isharvesting(
+            doscreenshot(monitor_number), template_mining, 0.7
+        ) | isharvesting(doscreenshot(monitor_number), template_mining_while_click, 0.7)
+
+        print("harvesting:", harvesting)
+        while harvesting is not True:
+            try:
+                clicktile(find(doscreenshot(monitor_number), template_ressource, 0.7))
+                clickharvest(find(doscreenshot(monitor_number), template_button, 0.6))
+                time.sleep(12)
+                harvesting = isharvesting(
+                    doscreenshot(monitor_number), template_mining, 0.7
+                ) | isharvesting(
+                    doscreenshot(monitor_number), template_mining_while_click, 0.7
+                )
+            except TypeError:
+                print("no more ressource here, leaving")
+                something_in_the_area = 0
+
+
+# main
 time.sleep(3)
-try:
-    clicktile(findtile(doscreenshot(monitornumber)))
-except TypeError:
-    print(
-        "then, we should try to move back to the starting point and continue our journey"
-    )
+something_in_the_area = find(doscreenshot(monitor_number), template_ressource, 0.7)
+explorezone()
